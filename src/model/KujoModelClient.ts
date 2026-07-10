bdeac6ed5516a071308f00b6abc6795a447df1ab
Robert DeVore <deviodigital@gmail.com>
2026-07-10 17:14:05 -0400

    feat: rebuild Tribunal as a Kujo-native tool

--- src/model/KujoModelClient.ts
+++
@@ -1,6 +0,0 @@
-import type { KujoModelRequest, KujoModelResponse } from "../types.js";
-
-/** Tribunal's only model invocation dependency. */
-export interface KujoModelClient {
-  invoke(request: KujoModelRequest): Promise<KujoModelResponse>;
-}
